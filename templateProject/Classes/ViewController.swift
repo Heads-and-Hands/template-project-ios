29043f628252459224d13ad891be43a2d291cc50
Eugene Sorokin <sorokin.evg.handh@gmail.com>
2018-09-25 13:10:06 +0300

    Add window

--- templateProject/Classes/ViewController.swift
+++ templateProject/Classes/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        view.backgroundColor = .white
+
         let label = UILabel()
         label.text = "Greeting!"
         label.translatesAutoresizingMaskIntoConstraints = false
